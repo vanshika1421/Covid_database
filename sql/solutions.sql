c71028e4677bb5bdae2ff0070d6333b8495e8e3b
vanshika <vanshika1407@gmail.com>
2026-09-17 11:15:18 +0530

    UC1 country with highest cases

---
+++ sql/solutions.sql
@@ -0,0 +1,10 @@
+SELECT
+    c.name AS country,
+    g.report_date,
+    g.confirmed
+FROM global_covid_stats g
+JOIN country c
+    ON g.country_id = c.country_id
+WHERE g.report_date = DATE '2021-09-30'
+ORDER BY g.confirmed DESC
+LIMIT 1;
