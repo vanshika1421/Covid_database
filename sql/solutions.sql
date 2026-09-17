c34ee5c527d7f18ccdb820356af7bc5cb8af5f74
vanshika <vanshika1407@gmail.com>
2026-09-17 11:20:57 +0530

    UC3 continent summary query:

--- sql/solutions.sql
+++ sql/solutions.sql
@@ -18,3 +18,15 @@ JOIN country c
 WHERE g.report_date = DATE '2021-09-30'
 GROUP BY c.name, g.report_date
 ORDER BY total_deaths DESC;
+
+--3. List the continents along with the total number of confirmed cases, deaths, and recoveries.
+SELECT 
+    c.continent,
+    SUM(cs.confirmed) AS confirmed_cases,
+    SUM(cs.deaths) AS deaths,
+    SUM(cs.recovered) AS recoveries
+FROM country c
+JOIN covid_case_stats cs
+    ON c.country_id = cs.country_id
+GROUP BY c.continent
+ORDER BY c.continent;
