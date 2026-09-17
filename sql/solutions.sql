83adfb927cda8f1614e81e012e685c91961d9d1a
vanshika <vanshika1407@gmail.com>
2026-09-17 11:18:18 +0530

    UC2 country-deaths

--- sql/solutions.sql
+++ sql/solutions.sql
@@ -8,3 +8,13 @@ JOIN country c
 WHERE g.report_date = DATE '2021-09-30'
 ORDER BY g.confirmed DESC
 LIMIT 1;
+SELECT
+    c.name AS country,
+    g.report_date,
+    SUM(g.deaths) AS total_deaths
+FROM global_covid_stats g
+JOIN country c
+    ON g.country_id = c.country_id
+WHERE g.report_date = DATE '2021-09-30'
+GROUP BY c.name, g.report_date
+ORDER BY total_deaths DESC;
